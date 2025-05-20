b2e204a72dc7dd496411e5afc50fc773d18235ea
Blaž Pavlica <blaz.pavlica@gmail.com>
2025-05-20 12:51:37 +0200

    trying to fix sourcemaps

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     base: "",
     plugins: [react(), tailwindcss()],
+    build: {
+        sourcemap: true, // enable production source maps
+    },
+    css: {
+        devSourcemap: true, // enable CSS source maps during development
+    },
 });
