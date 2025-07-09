21a69df8b475fa7368c1408425e483e5afa9d4f6
Blaž Pavlica <blaz.pavlica@sinergise.com>
2025-07-09 10:41:09 +0200

    Changed the logo, added a bit of responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
-const HeaderLogo = () => {
+const Header = () => {
     return (
-        <div className="flex items-center px-4">
-            <img
-                src={"./cdse-logo.png"}
-                className="sm:w-48 w-24 h-auto"
-                alt="logo"
-            />
-            <h1 className="heading-primary ml-8 mt-5 text-3xl">
-                <i className="text-white cursor-default"> PU Calculator</i>
-            </h1>
+        <div className="flex justify-center lg:flex-row flex-co px-4 py-3 lg:x-4 bg-blue-500">
+            <div className="flex items-center w-full 2xl:w-420 ps-4 2xl:ps-8">
+                <img
+                    src={"./logonew.svg"}
+                    className="sm:w-60 w-48 h-auto "
+                    alt="logo"
+                />
+                <h1 className="heading-primary ml-8 mt-3 text-2xl sm:text-3xl">
+                    <i className="text-white cursor-default"> PU Calculator</i>
+                </h1>
+            </div>
         </div>
     );
 };
 
-export default HeaderLogo;
+export default Header;
